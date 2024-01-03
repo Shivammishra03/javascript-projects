9c9c945aeb7d549651639690bbef40155f9281bc
Shivam Mishra <113275391+Shivammishra03@users.noreply.github.com>
2024-01-03 12:57:01 +0530

    counter project added

---
+++ counter-project/app.js
@@ -0,0 +1,32 @@
+let count = 0;
+//select value and button
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(item){
+    console.log(item);
+    item.addEventListener("click", function(e){
+        //get the clicked button value
+        // debugger
+        console.log(e.currentTarget.classList);
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }else if(styles.contains('increase')) {
+            count++;
+        }else {
+            count = 0;
+        }
+        if(count > 0) {
+            value.style.color = "green";
+        }
+        if(count < 0){
+            value.style.color = "red";
+        }
+        if(count == 0) {
+            value.style.display = 'black';
+        }
+        value.textContent = count;
+    })
+})
