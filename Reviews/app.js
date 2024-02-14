f220cb1e560cb781fb2a6af5d4c22a3804d02249
Shivam Mishra <113275391+Shivammishra03@users.noreply.github.com>
2024-02-14 22:47:22 +0530

    changes

--- Reviews/app.js
+++ Reviews/app.js
@@ -29,7 +29,6 @@ const reviews = [
       text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
     },
 ];
-
 // select items
 
 const img = document.getElementById("person-img");
